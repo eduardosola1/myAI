19d2971d9b6c4ed5a089396f5f18e903edf3d480
Eduardo Sola <eduardosolaburbano@gmail.com>
2025-03-04 11:38:54 -0500

    Update ui.ts

--- configuration/ui.ts
+++ configuration/ui.ts
@@ -4,7 +4,7 @@ export const CHAT_HEADER: string = `Charlie - Your AI-Powered Guide to Investing
 export const MESSAGE_PLACEHOLDER: string = `[MESSAGE PLACEHOLDER]`;
 export const FOOTER_MESSAGE: string = `Customized AI by Eduardo Sola`;
 export const CLEAR_BUTTON_TEXT: string = `Clear Chat`;
-export const PAGE_TITLE: string = `Charlie - AI Assistant`;
+export const PAGE_TITLE: string = `Charlie - AI-Powered Investing Guide`;
 export const PAGE_DESCRIPTION: string = `Chat with ${AI_NAME}, ${OWNER_NAME}'s AI assistant.`;
 
 export const EMPTY_CITATION_MESSAGE: string = "Unspecified source";
